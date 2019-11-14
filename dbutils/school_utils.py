8d8df839a1eb4f4d1870130d3e6cc83463b65ffa
Raul Burian <raulbc099@gmail.com>
2019-11-14 14:34:05 +0200

    Added db_api

--- dbutils/school_utils.py
+++ dbutils/school_utils.py
@@ -23,66 +23,71 @@ def get_user_preferences(username):
 
 
 def get_faculty_activities(*, prof=None, subject=None, type=None, spec=None):
-    if not prof and not subject and not type and not spec:#0
+    if not prof and not subject and not type and not spec:  # 0
         return [FacultyActivity(activity) for activity in SchoolActivity.objects.all()]
-    elif not subject and not type and not spec:#1
+    elif not subject and not type and not spec:  # 1
         return [FacultyActivity(activity) for activity in SchoolActivity.objects.filter(professor=prof)]
-    elif not prof and not type and not spec:#2
+    elif not prof and not type and not spec:  # 2
         return [FacultyActivity(activity) for activity in SchoolActivity.objects.filter(title=subject)]
-    elif not prof and not subject and not spec:#3
+    elif not prof and not subject and not spec:  # 3
         return [FacultyActivity(activity) for activity in SchoolActivity.objects.filter(type=type)]
-    elif not prof and not subject and not type:#4
-        activities=[]
+    elif not prof and not subject and not type:  # 4
+        activities = []
         for group in Specialization.groups[spec]:
             activities.extend([FacultyActivity(activity) for activity in SchoolActivity.objects.filter(group=group)])
         return activities
-    elif not type and not spec:#1 2
-        return [FacultyActivity(activity) for activity in SchoolActivity.objects.filter(professor=prof,title=subject)]
-    elif not subject and not spec:#1 3
-        return [FacultyActivity(activity) for activity in SchoolActivity.objects.filter(professor=prof,type=type)]
-    elif not subject and not type:#1 4
+    elif not type and not spec:  # 1 2
+        return [FacultyActivity(activity) for activity in SchoolActivity.objects.filter(professor=prof, title=subject)]
+    elif not subject and not spec:  # 1 3
+        return [FacultyActivity(activity) for activity in SchoolActivity.objects.filter(professor=prof, type=type)]
+    elif not subject and not type:  # 1 4
         activities = []
         for group in Specialization.groups[spec]:
-            activities.extend([FacultyActivity(activity) for activity in SchoolActivity.objects.filter(professor=prof,group=group)])
+            activities.extend(
+                [FacultyActivity(activity) for activity in SchoolActivity.objects.filter(professor=prof, group=group)])
         return activities
-    elif not prof and not spec:#2 3
+    elif not prof and not spec:  # 2 3
         return [FacultyActivity(activity) for activity in SchoolActivity.objects.filter(title=subject, type=type)]
-    elif not prof and not type:#2 4
+    elif not prof and not type:  # 2 4
         activities = []
         for group in Specialization.groups[spec]:
             activities.extend(
                 [FacultyActivity(activity) for activity in SchoolActivity.objects.filter(title=subject, group=group)])
         return activities
-    elif not prof and not subject:#3 4
+    elif not prof and not subject:  # 3 4
         activities = []
         for group in Specialization.groups[spec]:
             activities.extend(
                 [FacultyActivity(activity) for activity in SchoolActivity.objects.filter(type=type, group=group)])
         return activities
-    elif not spec:#1 2 3
-        return [FacultyActivity(activity) for activity in SchoolActivity.objects.filter(professor=prof,title=subject, type=type)]
-    elif not type:#1 2 4
+    elif not spec:  # 1 2 3
+        return [FacultyActivity(activity) for activity in
+                SchoolActivity.objects.filter(professor=prof, title=subject, type=type)]
+    elif not type:  # 1 2 4
         activities = []
         for group in Specialization.groups[spec]:
             activities.extend(
-                [FacultyActivity(activity) for activity in SchoolActivity.objects.filter(professor=prof,title=subject, group=group)])
+                [FacultyActivity(activity) for activity in
+                 SchoolActivity.objects.filter(professor=prof, title=subject, group=group)])
         return activities
-    elif not subject:#1 3 4
+    elif not subject:  # 1 3 4
         activities = []
         for group in Specialization.groups[spec]:
             activities.extend(
-                [FacultyActivity(activity) for activity in SchoolActivity.objects.filter(professor=prof,type=type, group=group)])
+                [FacultyActivity(activity) for activity in
+                 SchoolActivity.objects.filter(professor=prof, type=type, group=group)])
         return activities
-    elif not prof:#2 3 4
+    elif not prof:  # 2 3 4
         activities = []
         for group in Specialization.groups[spec]:
             activities.extend(
-                [FacultyActivity(activity) for activity in SchoolActivity.objects.filter(title=subject,type=type, group=group)])
+                [FacultyActivity(activity) for activity in
+                 SchoolActivity.objects.filter(title=subject, type=type, group=group)])
         return activities
     else:
         activities = []
         for group in Specialization.groups[spec]:
             activities.extend(
-                [FacultyActivity(activity) for activity in SchoolActivity.objects.filter(professor=prof,subject=subject,type=type, group=group)])
+                [FacultyActivity(activity) for activity in
+                 SchoolActivity.objects.filter(professor=prof, subject=subject, type=type, group=group)])
         return activities
-
