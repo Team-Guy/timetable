5d7d7492f9da0ff20586ef229abf6929499d8920
Ubuntu <ubuntu@ip-172-31-31-1.eu-west-1.compute.internal>
2019-11-15 12:20:33 +0000

    Stashed deploy changes

--- schedule/views.py
+++ schedule/views.py
@@ -15,18 +15,18 @@ def health(request):
 @api_view(['GET'])
 def user_schedule(request, username):
     odd_days_dict = dict(
-        monday=list(),
-        tuesday=list(),
-        wednesday=list(),
-        thursday=list(),
-        friday=list()
+        luni=list(),
+        marti=list(),
+        miercuri=list(),
+        joi=list(),
+        vineri=list()
     )
     even_days_dict = dict(
-        monday=list(),
-        tuesday=list(),
-        wednesday=list(),
-        thursday=list(),
-        friday=list()
+        luni=list(),
+        marti=list(),
+        miercuri=list(),
+        joi=list(),
+        vineri=list()
     )
     to_return = {1: odd_days_dict, 2: even_days_dict}
     school_act_qs = UserSchoolActivity.objects.filter(user__email=f'{username}@gmail.com')
@@ -37,7 +37,7 @@ def user_schedule(request, username):
         if frequency == 'full':
             to_return[1][day].append(activity_dict)
             to_return[2][day].append(activity_dict)
-        elif frequency == 'even':
+        elif frequency == 'par':
             to_return[2][day].append(activity_dict)
         else:
             to_return[1][day].append(activity_dict)
