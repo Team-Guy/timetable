081ddb1ed76a7119193967b67dd5aff5ef3a0369
Raul Burian <raulbc099@gmail.com>
2019-12-10 12:10:30 +0200

    Added edit/ get profile , get prefs

--- authentication/views.py
+++ authentication/views.py
@@ -2,11 +2,10 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from dbutils.optional import Optional
 from authentication.models import Preference
-from dbutils.school_utils import get_user_preferences,get_faculty_activities,remove_peda_sport
+from dbutils.school_utils import get_user_preferences, get_faculty_activities, remove_peda_sport
 from schedule.models import User, UserSchoolActivity, SchoolActivity
 from scrapping.main import getAll
 import json
-
 
 # Create your views here.
 
@@ -87,23 +86,27 @@ def preferences(request, username):
         pid = _process_preferences(request.body, username)
         ret = {"id": pid}
         return JsonResponse(ret)
-    elif request.method=="GET":
+    elif request.method == "GET":
         return JsonResponse(get_user_preferences(username).to_dict())
     else:
-        return JsonResponse({"id":-1})
+        return JsonResponse({"id": -1})
 
 
 def _process_optionals(post_body, username):
     post = json.loads(post_body)
     user = User.objects.get(email=username)
-    user.sport=post["sport"]
-    user.peda=post["peda"]
+    user.sport = post["sport"]
+    user.peda = post["peda"]
     optionale = []
-    for optional in post.optionals:
+    for optional in post['optionals']:
         optionale.append(optional)
     school_activities = SchoolActivity.objects.filter(group=user.group)
     for activity in school_activities:
         if activity.title not in Optional.optional:
+            if activity.title == Optional.sport and (not user.sport):
+                continue
+            if activity.title in Optional.peda and (not user.peda):
+                continue
             user_activity = UserSchoolActivity(
                 user=user,
                 school_activity=activity
@@ -131,22 +134,41 @@ def optionals(request, username):
     return JsonResponse(ret)
 
 
-def _process_edit_post(post_body,username):
-    user=User.objects.get(email=username)
-    post=json.loads(post_body)
-
+def _process_edit_post(post_body, username):
+    user = User.objects.get(email=username)
+    post = json.loads(post_body)
+    user.group = post['group']
     user.save()
+    UserSchoolActivity.objects.filter(user=user).delete()
+    _process_optionals(post_body, username)
     return user.id
 
 
+def _process_edit_get(username):
+    user = User.objects.get(email=username)
+    get = {}
+    get['group'] = user.group
+    get['sport'] = user.sport
+    get['peda'] = user.peda
+    get['optionals'] = []
+    for u_activity in UserSchoolActivity.objects.filter(user=user):
+        activity = u_activity.school_activity
+        print(activity.title)
+        if activity.title in Optional.optional and activity.title not in get['optionals']:
+            get['optionals'].append(activity.title)
+
+    return get
+
+
 @csrf_exempt
-def edit_profile(request,username):
-    username=username+"@gmail.com"
-    if request.method=="POST":
-        uid=_process_edit_post(request.body,username)
-        return JsonResponse({"id":uid})
-    if request.method=="GET":
-        return None
+def edit_profile(request, username):
+    username = username + "@gmail.com"
+    if request.method == "POST":
+        uid = _process_edit_post(request.body, username)
+        return JsonResponse({"id": uid})
+    if request.method == "GET":
+        return JsonResponse(_process_edit_get(username))
+
 
 @csrf_exempt
 def updateDB(request):
