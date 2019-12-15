f7247486726cc439103a9a55d1903a5fe915f342
Raul Burian <raulbc099@gmail.com>
2019-12-15 19:28:02 +0200

    register to admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from authentication.models import LastTimetable, Preference
+
+admin.site.register(LastTimetable)
+admin.site.register(Preference)
